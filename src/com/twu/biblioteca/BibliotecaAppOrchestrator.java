211bba615373e13b454993a6af8b4b9fabf15d13
marco-beduschi <je-0n-r0g-fea>
2020-04-08 20:45:56 -0300

    update BibliotecaAppOrchestrator to print the catalog book list

--- src/com/twu/biblioteca/BibliotecaAppOrchestrator.java
+++ src/com/twu/biblioteca/BibliotecaAppOrchestrator.java
@@ -1,9 +1,32 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.models.Book;
+
 import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BibliotecaAppOrchestrator {
     public static void run(PrintStream printStream) {
+
         printStream.println("Welcome to the Library!");
+        printStream.println("");
+        printStream.println("Books available:");
+        getBooksList().forEach(printStream::println);
+        printStream.println("");
+    }
+
+    public static List<String> getBooksList() {
+        BibliotecaCatalog catalog = BibliotecaCatalogFactory.build();
+
+        return IntStream.range(0, catalog.books.length)
+                .mapToObj(index -> {
+                    int bookNum = index + 1;
+                    Book currBook = catalog.books[index];
+
+                    return String.format("Book %d: %s, %s, %d.", bookNum, currBook.title, currBook.author, currBook.yearPublished);
+                })
+                .collect(Collectors.toList());
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppOrchestratorTest.java
+++ test/com/twu/biblioteca/BibliotecaAppOrchestratorTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import java.io.PrintStream;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -19,5 +18,19 @@ public class BibliotecaAppOrchestratorTest {
 
         // Then
         verify(printStream).println("Welcome to the Library!");
+    }
+
+    @Test
+    public void shouldPrintListOfBooks() {
+        // Given
+        PrintStream printStream = mock(PrintStream.class);
+
+        // When
+        BibliotecaAppOrchestrator.run(printStream);
+
+        // Then
+        verify(printStream).println("Book 1: Myth Of Sisyphus, Albert Camus, 1942.");
+        verify(printStream).println("Book 2: Sophie's World, Joseph Gaardner, 1992.");
+        verify(printStream).println("Book 3: Harry Potter, J.K. Rolling, 1992.");
     }
 }
